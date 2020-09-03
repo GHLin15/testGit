f12af9d3f2a87dffb2e9fdde12b4801b6212d890
liren15 <lllxzr15@163.com>
2020-09-03 17:05:13 +0800

    解决冲突

--- src/main/java/com/liren/git/HelloGit.java
+++ src/main/java/com/liren/git/HelloGit.java
@@ -6,11 +6,8 @@ public class HelloGit {
 		// TODO Auto-generated method stub
 		System.out.println("HelloGit");
 		System.out.println("Switch To");
-<<<<<<< HEAD
 		System.out.println("master add");
-=======
 		System.out.println("dev add");
->>>>>>> refs/heads/dev
 	}
 
 }
