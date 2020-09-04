92d4c5e2cc1079bbdd805b49571724dd1d0b1fb6
liren <lllxzr15@163.com>
2020-09-04 11:24:04 +0800

    liren1512上传的项目

--- src/main/java/com/liren/git/HelloGithub.java
+++ src/main/java/com/liren/git/HelloGithub.java
@@ -5,6 +5,6 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		System.out.println("通过Github克隆的项目");
 		System.out.println("通过Github其他用户上传的项目");
+		System.out.println("liren1512上传的项目");
 	}
-
 }
